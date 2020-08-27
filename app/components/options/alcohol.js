7de15879fb04fba7c07b9631393ba16abb7daa2a
Geoffroy <geoffroymounier@gmail.com>
2020-08-27 13:11:58 +0100

    use modif icon

--- app/components/options/alcohol.js
+++ app/components/options/alcohol.js
@@ -1,82 +1,74 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { Text, StyleSheet, ScrollView, TouchableOpacity, View, Dimensions, PanResponder } from 'react-native';
 import Animated, { interpolate } from "react-native-reanimated";
-import { PinchGestureHandler, State, PanGestureHandler } from 'react-native-gesture-handler'
+import { State, PanGestureHandler } from 'react-native-gesture-handler'
+import Svg, {
+    Stop,
+    LinearGradient,
+    Defs,
+    Polygon,
+    G
+} from 'react-native-svg'
 const { width, height } = Dimensions.get('window')
-const { event, block, min,max, cond, eq, Value, set, add, debug, concat, call } = Animated;
-
-const  useInterval = (callback, delay) => {
-    const savedCallback = useRef();
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
-  }
+const { event, block, min, max, cond, eq, Value, set, add, call } = Animated;
 
 const Alcohol = ({
 }) => {
-    const [state,setState] = useState(0)
+    const [state, setState] = useState(0)
+    const [svgHeight, setSvgHeight] = useState('0')
     const yValue = useRef(new Value(0)).current;
     const yOffset = useRef(new Value(0)).current;
 
-    const heightComponent = useRef()
+    const alcoholValue = useRef(new Value(0)).current;
     const minValue = new Value(-175)
     const maxValue = new Value(175)
     const heightValue = useRef(new Value(0)).current;
     const setValue = async ([height]) => {
-        setState(height)
+        setState(height.toFixed(1))
+        setSvgHeight((100 * (1 - (height / 24))).toFixed(0))
     }
     const gestureEvent = event([
         {
             nativeEvent: ({ translationX: x, translationY: y, state }) => block([
-                    set(yValue,min(maxValue,max(minValue,add(y,yOffset)))),
-                    set(heightValue,interpolate(yValue,{inputRange:[-175,175], outputRange:[350,50]})),
-                    debug('yvalue',yValue),
-                    call([heightValue],setValue),
-                    
-                    cond(
-                        eq(state, State.END), 
+                set(yValue, min(maxValue, max(minValue, add(y, yOffset)))),
+                set(heightValue, interpolate(yValue, { inputRange: [minValue, maxValue], outputRange: [350, 50] })),
+                set(alcoholValue, interpolate(heightValue, { inputRange: [50, 350], outputRange: [5, 24] })),
+                call([alcoholValue], setValue),
+                cond(
+                    eq(state, State.END),
                     [
-                        debug('end',yOffset),
-
                         set(yOffset, add(yOffset, y)),
-                        
                     ]
-                    )
-                ])
-            
+                )
+            ])
         }
     ])
-
-
     return (
         <>
-            <View style={{ width, height, justifyContent: 'center', alignItems: 'center',}}>
+            <View style={{ width, height, justifyContent: 'center', alignItems: 'center', }}>
                 <PanGestureHandler
                     onHandlerStateChange={gestureEvent}
                     onGestureEvent={gestureEvent} style={{ ...styles.box, backgroundColor: 'red' }}>
-                    <Animated.View
-                        style={[
-                            styles.box
-                        ]}
-                    ><Animated.View 
-                    ref={heightComponent}
-                    style={{height:heightValue, backgroundColor: "red"}}/>
+                    <Animated.View style={styles.box}
+                    ><Svg height="210" width="210">
+                            <Defs>
+                                <LinearGradient id="half" gradientTransform="rotate(90)">
+                                    <Stop offset="0%" stopColor="white" />
+                                    <Stop offset={`${svgHeight}%`} stopColor="white" />
+                                    <Stop offset={`${svgHeight}%`} stopColor="blue" />
+                                    <Stop offset="100%" stopColor="blue" />
+                                </LinearGradient>
+                            </Defs>
+                            <G fill="url(#half)" stroke="blue" strokeWidth="2">
+                                <Polygon points="100,10 40,198 190,78 10,78 160,198" />
+                                <Polygon points="100,10 40,198 190,78 10,78 160,198" stroke="none" />
+                            </G>
+                        </Svg>
                     </Animated.View>
-                   
                 </PanGestureHandler>
                 <Text>{state}</Text>
             </View>
         </>
-
     )
 }
 const styles = StyleSheet.create({
@@ -86,13 +78,7 @@ const styles = StyleSheet.create({
         fontFamily: "ProximaNova-Regular"
     },
     box: {
-        height: 350,
-        width: 150,
-        borderRadius: "back",
-        // overflow:'hidden',
-        borderWidth:2,
-        borderRadius: 5,
-        justifyContent:'flex-end'
+        justifyContent: 'center'
     },
     categoryTitle: {
         fontWeight: "600",
